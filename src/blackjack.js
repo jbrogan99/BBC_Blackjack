7583edc834fb901c5c1be06707464118f8ae3ed5
jbrogan99 <jack.brogan99@gmail.com>
2023-01-04 17:22:29 +0000

    updated data structure and check for duplicate cards

--- src/blackjack.js
+++ src/blackjack.js
@@ -1,4 +1,4 @@
-export const dealHand = () => {
+const dealHand = () => {
   let cards = {
     kingHeart: 10,
     queenHeart: 10,
@@ -7,13 +7,50 @@ export const dealHand = () => {
   };
 
   const cardsKeyValues = Object.entries(cards);
+  let handScore = {};
   let twoCards = [];
   let count = 0;
   while (count < 2) {
+    // choose random card
     let randomCard =
       cardsKeyValues[Math.floor(Math.random() * cardsKeyValues.length)];
-    twoCards.push(randomCard);
-    count++;
+
+    // get name of card
+    let nameOfCard = randomCard[0];
+    // return card if matches with current card in deck
+    const duplicateCards = twoCards.filter((cardsInDeck, index) => {
+      return cardsInDeck[0] === nameOfCard;
+    });
+    // checks if duplicate card is true
+    if (duplicateCards.length === 0) {
+      count++;
+      twoCards.push(randomCard);
+    }
   }
-  return twoCards;
+  //convert array into array of objects, easier to work with
+  let hand = twoCards.map((el) => {
+    return {
+      name: el[0],
+      value: el[1],
+    };
+  });
+  handScore["hand"] = hand;
+  console.log(handScore);
+
+  // data structure example
+  // {
+  //   hand: [
+  //     {
+  //       name: '5hearts',
+  //       value: 5
+  //     },
+  //     {
+  //       name: '5hearts',
+  //       value: 5
+  //     }
+  //   ],
+  //   score: 10
+  // }
 };
+
+dealHand();
