f052ac33a20d787bc1113d1b9bec6bf634efed5a
jbrogan99 <jack.brogan99@gmail.com>
2023-01-05 18:57:27 +0000

    refactored code deal hand

--- src/blackjack.js
+++ src/blackjack.js
@@ -1,56 +1,63 @@
-const dealHand = () => {
-  let cards = {
-    kingHeart: 10,
-    queenHeart: 10,
-    eightHeart: 8,
-    nineHeart: 9,
-  };
+// data structure example
+// {
+//   hand: [ '5hearts', 5],  ['5clubs',  5],
+//   score: 10
+// }
 
-  const cardsKeyValues = Object.entries(cards);
-  let handScore = {};
-  let twoCards = [];
-  let count = 0;
-  while (count < 2) {
-    // choose random card
-    let randomCard =
-      cardsKeyValues[Math.floor(Math.random() * cardsKeyValues.length)];
-
-    // get name of card
-    let nameOfCard = randomCard[0];
-    // return card if matches with current card in deck
-    const duplicateCards = twoCards.filter((cardsInDeck, index) => {
-      return cardsInDeck[0] === nameOfCard;
-    });
-    // checks if duplicate card is true
-    if (duplicateCards.length === 0) {
-      count++;
-      twoCards.push(randomCard);
-    }
-  }
-  //convert array into array of objects, easier to work with
-  let hand = twoCards.map((el) => {
-    return {
-      name: el[0],
-      value: el[1],
-    };
-  });
-  handScore["hand"] = hand;
-  console.log(handScore);
-
-  // data structure example
-  // {
-  //   hand: [
-  //     {
-  //       name: '5hearts',
-  //       value: 5
-  //     },
-  //     {
-  //       name: '5hearts',
-  //       value: 5
-  //     }
-  //   ],
-  //   score: 10
-  // }
+const deck = {
+  kingHeart: 10,
+  queenHeart: 10,
+  eightHeart: 8,
+  nineHeart: 9,
 };
 
-dealHand();
+let currentHand = {
+  hand: [],
+  score: 0,
+};
+
+const dealCard = (card) => {
+  const cardsKeyValues = Object.entries(deck);
+
+  let randomCard =
+    cardsKeyValues[Math.floor(Math.random() * cardsKeyValues.length)];
+
+  card.hand.push(randomCard);
+
+  let score = card.hand.reduce((acc, curr) => acc + curr[1], 0);
+  card.score = score;
+
+  let nameOfCard = randomCard[0];
+  delete deck[nameOfCard];
+};
+
+const dealHand = (card) => {
+  let count = 0;
+  while (count < 2) {
+    dealCard(card);
+    count++;
+  }
+  return currentHand;
+};
+
+const hit = (card) => {
+  dealCard(card);
+  return currentHand;
+};
+
+console.log("hand", dealHand(currentHand));
+console.log("hit", hit(currentHand));
+// dealCard();
+// hit();
+// let x = hit();
+// console.log("hit", x);
+// dealHand();
+// hit();
+
+// const hit = (firstHand) => {};
+
+// const hit = (currentScore) => {
+
+// };
+
+// console.log("hit", hit(firstHand.score));
--- test/blackjack.test.js
+++ test/blackjack.test.js
@@ -1,7 +1,11 @@
-import { dealHand } from "../src/blackjack.js";
+import { dealHand, hit } from "../src/blackjack.js";
+
+let handAndScore = dealHand();
 
 describe("In blackjack", () => {
   test("I am dealt my opening hand, I should have two cards", () => {
-    expect(Object.entries(dealHand())).toHaveLength(2);
+    expect(handAndScore.hand).toHaveLength(2);
   });
+
+  test("When I choose hit, then I receive another card and my score is updated", () => {});
 });
